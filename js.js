cd96e573407bc6acde732c0e34c28d3b2b8b13db
sambhavjyoti <156454913+sambhavjyoti@users.noreply.github.com>
2024-01-12 22:05:50 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,36 @@
+let string="";
+let buttons = document.querySelectorAll('.button');
+
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click', (e)=>{
+        if(e.target.innerHTML =='='){
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+
+        else if(e.target.innerHTML =='C'){
+            string = "Clear";
+            document.querySelector('input').value = string;
+        } 
+
+        else if(e.target.innerHTML =='M+'){
+            string = eval(string+1);
+            document.querySelector('input').value = string;
+        } 
+
+        else if(e.target.innerHTML =='M-'){
+            string = eval(string-1);
+            document.querySelector('input').value = string;
+        } 
+
+        else{
+            console.log(e.target);
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value =string;
+        }
+    })
+})
+
+/*make m+ and m- functional*/
+
+/*use try and catch for error in calculating */ 
